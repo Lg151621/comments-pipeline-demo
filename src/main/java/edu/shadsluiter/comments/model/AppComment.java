b8ca3dca16c06464db1a36f39e89c6cf1c3aaad9
Lg151621 <destrok21@gmail.com>
2025-12-09 11:07:32 -0700

    v3: Added timestamps to comments

--- src/main/java/edu/shadsluiter/comments/model/AppComment.java
+++ src/main/java/edu/shadsluiter/comments/model/AppComment.java
@@ -1,25 +1,53 @@
 package edu.shadsluiter.comments.model;
 
 import java.time.LocalDateTime;
- 
 
 public class AppComment {
+
     private int id;
     private String author;
-    private String content; 
+    private String content;
+    private LocalDateTime timestamp;
 
-    public AppComment(int id, String author, String content ) {
+    // Constructor used in initialData (with explicit id)
+    public AppComment(int id, String author, String content) {
         this.id = id;
         this.author = author;
-        this.content = content; 
+        this.content = content;
+        this.timestamp = LocalDateTime.now(); // set timestamp when created
     }
+
+    // Constructor used in addComment(String author, String content)
+    public AppComment(int id, String author, String content, LocalDateTime timestamp) {
+        this.id = id;
+        this.author = author;
+        this.content = content;
+        this.timestamp = (timestamp != null) ? timestamp : LocalDateTime.now();
+    }
+
+    // Optional convenience constructor (if you ever want to create without id)
+    public AppComment(String author, String content) {
+        this.author = author;
+        this.content = content;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    // Getters and setters
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getAuthor() {
         return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 
     public String getContent() {
@@ -30,24 +58,21 @@ public class AppComment {
         this.content = content;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
+    public LocalDateTime getTimestamp() {
+        return timestamp;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
     }
- 
-    
 
     @Override
-
     public String toString() {
-        return "Comment{" +
+        return "AppComment{" +
                 "id=" + id +
                 ", author='" + author + '\'' +
-                ", content='" + content + '\'' + 
+                ", content='" + content + '\'' +
+                ", timestamp=" + timestamp +
                 '}';
     }
-    
 }
